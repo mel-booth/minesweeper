0530fb826f9bc30cb03fdbde8dafb12c6590a86d
Mel Booth <mel.m.booth@gmail.com>
2016-09-25 10:48:43 +1300

    add auto generateBoard function and prompt for user to choose boardSize

--- minesweeper.js
+++ minesweeper.js
@@ -1,38 +1,43 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
-var board = {
-  cells: [
-    {row: 0, col: 0, isMine:true, hidden: true},
-    {row: 0, col: 1, isMine:false, hidden: true},
-    {row: 0, col: 2, isMine:false, hidden: true},
-    {row: 0, col: 3, isMine:false, hidden: true},
-    {row: 0, col: 4, isMine:false, hidden: true},
-    {row: 1, col: 0, isMine:false, hidden: true},
-    {row: 1, col: 1, isMine:false, hidden: true},
-    {row: 1, col: 2, isMine:false, hidden: true},
-    {row: 1, col: 3, isMine:true, hidden: true},
-    {row: 1, col: 4, isMine:true, hidden: true},
-    {row: 2, col: 0, isMine:false, hidden: true},
-    {row: 2, col: 1, isMine:false, hidden: true},
-    {row: 2, col: 2, isMine:true, hidden: true},
-    {row: 2, col: 3, isMine:false, hidden: true},
-    {row: 2, col: 4, isMine:false, hidden: true},
-    {row: 3, col: 0, isMine:true, hidden: true},
-    {row: 3, col: 1, isMine:false, hidden: true},
-    {row: 3, col: 2, isMine:false, hidden: true},
-    {row: 3, col: 3, isMine:true, hidden: true},
-    {row: 3, col: 4, isMine:false, hidden: true},
-    {row: 4, col: 0, isMine:false, hidden: true},
-    {row: 4, col: 1, isMine:false, hidden: true},
-    {row: 4, col: 2, isMine:true, hidden: true},
-    {row: 4, col: 3, isMine:false, hidden: true},
-    {row: 4, col: 4, isMine:false, hidden: true},
+var board = new Object()
+  board.cells = [
+    //{row: 0, col: 0, isMine:true, hidden: true},
+    //{row: 0, col: 1, isMine:false, hidden: true},
+    //{row: 0, col: 2, isMine:false, hidden: true},
+    //{row: 0, col: 3, isMine:false, hidden: true},
+    //{row: 0, col: 4, isMine:false, hidden: true},
+    //{row: 1, col: 0, isMine:false, hidden: true},
+    //{row: 1, col: 1, isMine:false, hidden: true},
+    //{row: 1, col: 2, isMine:false, hidden: true},
+    //{row: 1, col: 3, isMine:true, hidden: true},
+    //{row: 1, col: 4, isMine:true, hidden: true},
+    //{row: 2, col: 0, isMine:false, hidden: true},
+    //{row: 2, col: 1, isMine:false, hidden: true},
+    //{row: 2, col: 2, isMine:true, hidden: true},
+    //{row: 2, col: 3, isMine:false, hidden: true},
+    //{row: 2, col: 4, isMine:false, hidden: true},
+    //{row: 3, col: 0, isMine:true, hidden: true},
+    //{row: 3, col: 1, isMine:false, hidden: true},
+    //{row: 3, col: 2, isMine:false, hidden: true},
+    //{row: 3, col: 3, isMine:true, hidden: true},
+    //{row: 3, col: 4, isMine:false, hidden: true},
+    //{row: 4, col: 0, isMine:false, hidden: true},
+    //{row: 4, col: 1, isMine:false, hidden: true},
+    //{row: 4, col: 2, isMine:true, hidden: true},
+    //{row: 4, col: 3, isMine:false, hidden: true},
+    //{row: 4, col: 4, isMine:false, hidden: true},
 
-  ]
-}
+  ];
 
 function startGame () {
+
+  var row = boardSize ()
+  var col = row
+
+  createBoard (row, col);
+
   for (var i = 0; i < board.cells.length; i++){
     board.cells[i].surroundingMines = countSurroundingMines(board.cells[i]);
   }
@@ -41,8 +46,42 @@ function startGame () {
   lib.initBoard()
 
   document.addEventListener("click", checkForWin);
-  document.addEventListener(contextmenu, checkForWin);
+  document.addEventListener("contextmenu", checkForWin);
 }
+
+//AUTOMATICALLY GENERATE THE GAME BOARD...
+
+function createBoard (row, col) {
+  for (var a = 0; a < row; a++) {
+    for (var b = 0; b < col; b++) {
+      board.cells.push({
+        row: a,
+        col: b,
+        isMine: Boolean(Math.floor(Math.random()*1.3)),
+        isMarked: false,
+        hidden: true
+      })
+      console.log(board.cells)
+    }
+  }
+}
+
+//PROMPT FOR USER TO CHOOSE BOARD SIZE
+
+var boardSize = function() {
+
+  var askSize = prompt("Pick a lucky number between 3 and 6!");
+
+    if (askSize < 3 || askSize > 6) {
+        alert("I SAID choose a number BETWEEN 3 and 6, silly! Try again...");
+        location.reload();
+      }
+
+  else {
+    return askSize;
+  }
+}
+
 
 // Define this function to look for a win condition:
 //
